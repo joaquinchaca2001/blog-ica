63f3d2b80c07a622b35cc6cea0cd0103545fa58c
joaquinchaca2001 <joaquinchaca2001@hotmail.com>
2024-06-06 13:08:16 -0500

    Update and rename script.js.txt to script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('contact-form').addEventListener('submit', function(e) {
     e.preventDefault();
-    alert('¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.');
+    alert('Â¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.');
 });
